e89e4616582853702804d6a0d677acde43aeb5e0
Alexa Accuardi <16708253+NotAmazonAlexa@users.noreply.github.com>
2022-02-14 20:57:59 -0800

    exercise 5

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -56,8 +56,8 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow: auto;
 
   @media ${QUERIES.tabletAndSmaller} {
     border-top: 4px solid ${COLORS.gray[900]};
@@ -73,7 +73,10 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(
+    1rem,
+    8.1vw - 4rem,
+    3rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndSmaller} {
